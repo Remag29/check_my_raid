21a020919a70d36f6196e9f2866b3944cfb852a7
Remag29 <73289897+Remag29@users.noreply.github.com>
2024-05-05 20:53:15 +0200

    chore: Update RAID status notification logic and markdown formatting

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+# Function ############################################################################
 
 def read_mdstat_file(file_path) -> str:
     """Read the content of the mdstat file."""
@@ -20,8 +21,7 @@ def read_mdstat_file(file_path) -> str:
         print(f"The file {file_path} does not exist.")
         exit(1)
 
-
-def send_ntfy_notification(url, headers, data, auth):
+def send_ntfy_notification(url, headers, data, auth) -> None:
     """Send an HTTP POST request to trigger a notification on NTFY."""
     response = requests.post(url, headers=headers,
                              data=data.encode('utf-8'), auth=auth)
@@ -30,6 +30,59 @@ def send_ntfy_notification(url, headers, data, auth):
     else:
         print("Échec de l'envoi de la notification.")
         print(f"Failed to sent request. Status code: {response.status_code}")
+
+def get_match(mdstat_content, pattern) -> list:
+    """Search for the match in the mdstat file."""
+    matches = re.findall(pattern, mdstat_content)
+    if matches:
+        return matches
+    else:
+        return None
+
+def markdown_factory(matchs) -> str:
+    """Create a markdown message from the match."""
+    message = "# RAID report\n"
+
+    for match in matchs:
+        # Get the content of the match
+        mountPoint, raidState, raidType, disksList, total, other, diskState, other2 = match
+
+        # Get failed disk
+        disks = disksList.split(" ")
+        failedDisks = []
+        for index, char in enumerate(diskState):
+            if char == "_":
+                failedDisks.append(disks[index])
+
+        # Change the message according to the type
+        if failedDisks:
+            message += f"## *{mountPoint}* RAID status: KO\n"
+            message += f"> **Failed disks**: {', '.join(failedDisks)}\n"
+        else:
+            message += f"## *{mountPoint}* RAID status: OK\n"
+            message += f"All disks are operational !\n"
+
+        # Add the informations
+        message += f"### Informations :\n"
+        message += f"- **RAID type**: {raidType}\n"
+        message += f"- **RAID state**: {raidState}\n"
+        message += f"- **Disks list**: {', '.join(disks)}\n"
+        message += f"- **Total**: {total}\n"
+        message += "\n"
+    
+    return message
+
+def is_all_disks_ok(matchs) -> bool:
+    """Check if all disks are OK."""
+    for match in matchs:
+        # Get the content of the match
+        mountPoint, raidState, raidType, disksList, total, other, diskState, other2 = match
+
+        # Check if diskState contains "_"
+        if "_" in diskState:
+            return False
+        
+    return True
 
 def main():
 
@@ -42,61 +95,64 @@ def main():
     mdstat_content = read_mdstat_file(mdstat_file)
 
     # ReGex pattern
-    pattern = r" +\d+ \w+ \w+ [0-9.]+ \[\d\/\d\] \[([U_]+)\]"
+    pattern = r"(\w+) : (\w+) (raid\d+) (.+)\n +(\d+) (.+) \[\d+\/\d+\] \[([U_]+)\]\n + \w+: \d+\/\d+ (.+)"
 
     # Search for the match in the mdstat file
-    matches = re.findall(pattern, mdstat_content)
-    if matches:
-        # Print the result of the regex
-        content = matches[0]
-        print(f"Résultat de la regex : {content}")
+    matches = get_match(mdstat_content, pattern)
 
-        # Check if the content is "UU"
-        if content == "UU":
-            print("RAID status: OK")
+    # Get status of the RAID
+    raids_status = is_all_disks_ok(matches)
 
-            # NTFY notification
-            headers = {
-                "Title": "RAID OK",
-                "Priority": "low",
-                "Tags": "white_check_mark,cd"
-            }
-            data = "CheckMyRaid doesn't detect any anomaly on the RAID"
-            auth = ("", ntfy_token)
+    # Generate the markdown message
+    markdown = markdown_factory(matches)
 
-            send_ntfy_notification(ntfy_url, headers, data, auth)
+    if raids_status:
+        print("RAID status: OK")
 
-        else:
-            print("RAID status: KO")
+        # NTFY notification variable
+        headers = {
+            "Markdown": "yes",
+            "Title": "Check My Raid!",
+            "Priority": "low",
+            "Tags": "white_check_mark,cd"
+        }
+        data = markdown
+        auth = ("", ntfy_token)
 
-            # NTFY notification
-            headers = {
-                "Title": "Anomalie RAID",
-                "Priority": "max",
-                "Tags": "warning,cd"
-            }
-            data = "An anomaly has been detected on the RAID. Please check the RAID status as soon as possible."
-            auth = ("", ntfy_token)
-
-            send_ntfy_notification(ntfy_url, headers, data, auth)
     else:
-        print("No match found. Verify the content of the mdstat file.")
-    print("")
+        print("RAID status: KO")
+        # NTFY notification variable
+        headers = {
+            "Markdown": "yes",
+            "Title": "Check My Raid!",
+            "Priority": "high",
+            "Tags": "warning,cd"
+        }
+        data = markdown
+        auth = ("", ntfy_token)
 
+    # Send the notification
+    send_ntfy_notification(ntfy_url, headers, data, auth)
+
+#######################################################################################
 # Start the script 1 time on launch
 main()
 
-# Schedule the script to run at a specific time
+# Get the timezone and the schedule time
 timezone = os.getenv('TZ', 'UTC')
 trigerAt = os.getenv('TRIGER_SCHEDULE_AT')
 
+# Get the current date and time on the timezone
 tz = pytz.timezone(timezone)
 now = datetime.datetime.now(tz)
 
 print("Date and time is currently on the timezone", timezone, ":", now.strftime("%Y-%m-%d %H:%M:%S"))
 print(f"Next raid check scheduled at {trigerAt} \n")
+
+# Schedule the script to run at a specific time
 schedule.every().day.at(trigerAt, timezone).do(main)
 
+# Schedule loop
 while True:
     schedule.run_pending()
     time.sleep(1)
